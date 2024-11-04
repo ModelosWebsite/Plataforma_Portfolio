0a5e8bc87b9dd2d171359562b7c1abfd179cc820
fortcodeproject <pachecofortcodedig0@gmail.com>
2024-11-04 18:03:46 +0100

    Corrigir assets 04/11/2024

--- resources/views/layouts/config/app.blade.php
+++ resources/views/layouts/config/app.blade.php
@@ -10,7 +10,7 @@
     <title>@yield("title")</title>
 
     <!-- Custom fonts for this template-->
-    <link href={{asset("sbadmin/vendor/fontawesome-free/css/all.min.css")}}" rel="stylesheet" type="text/css">
+    <link href={{asset("Sbadmin/vendor/fontawesome-free/css/all.min.css")}}" rel="stylesheet" type="text/css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <link
@@ -18,7 +18,7 @@
         rel="stylesheet">
     @livewireStyles
     <!-- Custom styles for this template-->
-    <link href="{{asset("sbadmin/css/sb-admin-2.min.css")}}" rel="stylesheet">
+    <link href="{{asset("Sbadmin/css/sb-admin-2.min.css")}}" rel="stylesheet">
 </head>
 <body id="page-top">
     @include("sbadmin.help.App")
@@ -36,14 +36,14 @@
     <!-- End of Page Wrapper -->
 
     <!-- Bootstrap core JavaScript-->
-    <script src="{{asset("sbadmin/vendor/jquery/jquery.min.js")}}"></script>
-    <script src="{{asset("sbadmin/vendor/bootstrap/js/bootstrap.bundle.min.js")}}"></script>
+    <script src="{{asset("Sbadmin/vendor/jquery/jquery.min.js")}}"></script>
+    <script src="{{asset("Sbadmin/vendor/bootstrap/js/bootstrap.bundle.min.js")}}"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="{{asset("sbadmin/vendor/jquery-easing/jquery.easing.min.js")}}"></script>
+    <script src="{{asset("Sbadmin/vendor/jquery-easing/jquery.easing.min.js")}}"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="{{asset("sbadmin/js/sb-admin-2.min.js")}}"></script>
+    <script src="{{asset("Sbadmin/js/sb-admin-2.min.js")}}"></script>
     @livewireScripts
 
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
