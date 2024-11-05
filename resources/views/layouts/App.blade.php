98b7914362a40e4d407f5ac5b73e95b6a77abe70
fortcodeproject <pachecofortcodedig0@gmail.com>
2024-11-05 14:35:48 +0100

    Erros de Assets

--- resources/views/layouts/App.blade.php
+++ resources/views/layouts/App.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
@@ -8,34 +7,39 @@
   <title>@yield("title")</title>
 
   <!-- Vendor CSS Files -->
-  <link href="{{asset("assets/vendor/bootstrap/css/bootstrap.min.css")}}" rel="stylesheet">
-  <link href="{{asset("assets/vendor/bootstrap-icons/bootstrap-icons.css")}}" rel="stylesheet">
-  <link href="{{asset("assets/vendor/glightbox/css/glightbox.min.css")}}" rel="stylesheet">
-  <link href="{{asset("assets/vendor/swiper/swiper-bundle.min.css")}}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="{{asset("assets/css/style.css")}}" rel="stylesheet">
+  <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
   @livewireStyles
 </head>
 
 <body>
-
-    @yield("content")
+  @yield("content")
 
   <div id="preloader"></div>
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center">
+    <i class="bi bi-arrow-up-short"></i>
+  </a>
 
   <!-- Vendor JS Files -->
-  <script src="{{asset("assets/vendor/purecounter/purecounter_vanilla.js")}}"></script>
-  <script src="{{asset("assets/vendor/bootstrap/js/bootstrap.bundle.min.js")}}"></script>
-  <script src="{{asset("assets/vendor/glightbox/js/glightbox.min.js")}}"></script>
-  <script src="{{asset("assets/vendor/swiper/swiper-bundle.min.js")}}"></script>
-  <script src="{{asset("assets/vendor/typed.js/typed.umd.js")}}"></script>
+  <script src="{{ asset('assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
+  <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+  <script src="{{ asset('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+  <script src="{{ asset('assets/vendor/typed.js/typed.umd.js') }}"></script>
 
   <!-- Template Main JS File -->
-  <script src="{{asset("assets/js/main.js")}}"></script>
-  {{-- <script src="{{asset("assets/js/cookies.js")}}"></script> --}}
+  <script src="{{ asset('assets/js/main.js') }}"></script>
+  {{-- Caso necessário, descomente cookies.js depois de corrigir declarações duplicadas de Cookies --}}
+  {{-- <script src="{{ asset('assets/js/cookies.js') }}"></script> --}}
+  
+  <!-- jQuery -->
   <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+  
   @livewireScripts
   <x-livewire-alert::scripts />
 </body>
